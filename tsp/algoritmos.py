b4810297ca46ecaddbbe0dab6f75ec0050ab7caa
Carlos Bautista <cbautistap92@gmail.com>
2020-11-04 01:57:06 +0000

    se agregan modulos tsp -contiene clases- y algoritmos -contiene clases algoritmos-

---
+++ tsp/algoritmos.py
@@ -0,0 +1,18 @@
+"""
+Algoritmos disponibles:
+
+    - Ant Colony Optimization (aoc)
+    - greedy
+"""
+
+class algoritmo:
+    pass
+
+class greedy(algoritmo):
+    pass
+
+class aoc(algoritmo):
+    pass
+
+class ant:
+    pass
---
+++ tsp/tsp.py
@@ -0,0 +1,16 @@
+"""
+TSP
+"""
+
+class mundo:
+    pass
+
+class lugar:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class optimizador:
+    pass
+
+
