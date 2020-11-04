93d30e5351183e03acbc38f616b53aecafcf35c0
Carlos Bautista <cbautistap92@gmail.com>
2020-11-04 02:41:54 +0000

    Primer pasado, se agregan clases en tsp.py

--- tsp/tsp.py
+++ tsp/tsp.py
@@ -3,14 +3,35 @@ TSP
 """
 
 class mundo:
-    pass
+    def __init__(self, lugares):
+        self.lugares = []
+        with open(lugares, 'r') as lugares:
+            posicion = lugares.read()
+            self.lugares.append(lugar(*lugar))
 
 class lugar:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class optimizador:
-    pass
+    def __repr__(self):
+        return f"Posición: ({self.x}, {self.y})"
 
+class ruta:
+    def __init__(self):
+        self.lugares = []
+
+    @property
+    def costo(self):
+        pass
+
+    def __repr__(self):
+        return f"Ruta: {self.lugares} (Costo: {self.costo})"
+
+class optimizador:
+    def optimizar_ruta(self, lugares, algoritmo='greedy'):
+        mundo = mundo(lugares)
+        algoritmo = algoritmo()
+        ruta = algoritmo.ejecutar()
+        return ruta
 
